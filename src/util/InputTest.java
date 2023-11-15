ef978818478f48eaf91388bf54e8d9ecb8bcd583
stephaniezvill <stephanie.zoz.villarreal@gmail.com>
2023-11-15 09:06:45 -0600

    Finished exercise

--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -6,18 +6,18 @@ public class InputTest {
 
     public static void main(String[] args) {
         Input input = new Input();
-//        String theString = input.getString();
-//        System.out.println(theString);
+        String theString = input.getString();
+        System.out.println(theString);
 
-//        boolean userChoice = input.yesNo();
-//        System.out.println(userChoice);
-//
-//        int userNumber = input.getInt(1, 4);
-//        System.out.println(userNumber);
+        boolean userChoice = input.yesNo();
+        System.out.println(userChoice);
 
-//        System.out.println(input.getInt());
+        int userNumber = input.getInt(1, 4);
+        System.out.println(userNumber);
 
-//        System.out.println(input.getDouble(4.5, 9.6));
+        System.out.println(input.getInt());
+
+        System.out.println(input.getDouble(4.5, 9.6));
 
         System.out.println(input.getDouble());
     }
